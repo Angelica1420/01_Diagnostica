5991075ad38ea286b1da82c39043fe46e1e168e2
AngyD <angydordo@gmail.com>
2026-08-31 09:43:15 -0500

    feat(bloque 1): Casi terminado

--- bloque_1/src/bloque_1.ino
+++ bloque_1/src/bloque_1.ino
@@ -5,22 +5,27 @@ const int pinPulsador = 4;
 void setup() {
   Serial.begin(115200);
   // TODO: Configurar los pines de los LEDs como OUTPUT
-  
+  pinMode(23, OUTPUT); // Pin 23 (LED Rojo) enviará voltaje (3.3V o 0V)
+  pinMode(22, OUTPUT); // Pin 22 (LED Verde) enviará voltaje
+  pinMode(4, INPUT);   // Pin 4 (Pulsador) leerá voltaje digital
+}
   // TODO: Configurar el pin del pulsador como INPUT
   
-}
+
 
 void loop() {
   // TODO: Leer el estado del pulsador usando digitalRead
-  int estadoPulsador = 0; // Cambia esto
+  int estadoPulsador =  digitalRead(4); // Cambia esto
   
   if (estadoPulsador == HIGH) {
     // TODO: Encender LED Rojo y apagar LED Verde
-    
+    digitalWrite(23, HIGH);   
+    digitalWrite(22, LOW); 
     Serial.println("Pulsador PRESIONADO: LED Rojo ENCENDIDO");
   } else {
     // TODO: Apagar LED Rojo y encender LED Verde
-    
+    digitalWrite(23, LOW);     
+    digitalWrite(22, HIGH);  
     Serial.println("Pulsador SOLTADO: LED Verde ENCENDIDO");
   }
   
