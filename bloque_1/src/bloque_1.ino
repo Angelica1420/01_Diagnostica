f6d56f24a1b0a7759d87f72380a806589e8f6e4d
Angelica ordonez <andydordo@gmail.com>
2026-09-05 14:25:46 -0500

    fix(bloque1): corregir lógica de leds

--- bloque_1/src/bloque_1.ino
+++ bloque_1/src/bloque_1.ino
@@ -1,31 +1,32 @@
+#include <Arduino.h>
+
 const int pinLedRojo = 23;
 const int pinLedVerde = 22;
 const int pinPulsador = 4;
 
 void setup() {
   Serial.begin(115200);
-  // TODO: Configurar los pines de los LEDs como OUTPUT
-  pinMode(23, OUTPUT); // Pin 23 (LED Rojo) enviará voltaje (3.3V o 0V)
-  pinMode(22, OUTPUT); // Pin 22 (LED Verde) enviará voltaje
-  pinMode(4, INPUT);   // Pin 4 (Pulsador) leerá voltaje digital
-}
-  // TODO: Configurar el pin del pulsador como INPUT
   
-
+  pinMode(pinLedRojo, OUTPUT);
+  pinMode(pinLedVerde, OUTPUT);
+  
+  pinMode(pinPulsador, INPUT);
+}
 
 void loop() {
-  // TODO: Leer el estado del pulsador usando digitalRead
-  int estadoPulsador =  digitalRead(4); // Cambia esto
+  int estadoPulsador = digitalRead(pinPulsador);
   
   if (estadoPulsador == HIGH) {
-    // TODO: Encender LED Rojo y apagar LED Verde
-    digitalWrite(23, HIGH);   
-    digitalWrite(22, LOW); 
+    
+    digitalWrite(pinLedRojo, HIGH);
+    digitalWrite(pinLedVerde, LOW);
+    
     Serial.println("Pulsador PRESIONADO: LED Rojo ENCENDIDO");
   } else {
-    // TODO: Apagar LED Rojo y encender LED Verde
-    digitalWrite(23, LOW);     
-    digitalWrite(22, HIGH);  
+    
+    digitalWrite(pinLedRojo, LOW);
+    digitalWrite(pinLedVerde, HIGH);
+    
     Serial.println("Pulsador SOLTADO: LED Verde ENCENDIDO");
   }
   
